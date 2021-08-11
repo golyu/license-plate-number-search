898603d5694482aefee89ea8f3196251705c853d
golyu <lvzhongyiforchrome@gmail.com>
2021-08-11 16:31:27 +0800

    :art: 使用最新的vue3.2的语法糖,setup sugar

---
+++ src/component/keyboard-car.vue
@@ -0,0 +1,103 @@
+<template>
+  <view :class="`keyboard${props.show ? ' show' : ''}`">
+    <div>????</div>
+    <text key="{index}">沃日</text>
+  </view>
+</template>
+
+<script lang="ts">
+//车牌号键盘
+export default {
+  name: 'keyboard-car',
+}
+</script>
+<script lang="ts" setup>
+const props = defineProps<{ show: boolean }>();
+</script>
+
+
+<style lang="scss">
+.keyboard {
+  background-color: #ddd;
+  position: fixed;
+  z-index: 10000;
+  bottom: 0;
+  display: none;
+  width: 100%;
+  justify-content: center;
+
+  &.show {
+    display: flex;
+  }
+
+  .keyboard-ul {
+    display: flex;
+    text-align: center;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 1%;
+    position: relative;
+
+    .li {
+      display: block;
+      width: 10%;
+      height: 80px;
+      font-size: 28px;
+      justify-content: center;
+      line-height: 80px;
+      float: left;
+
+      .div:active {
+        opacity: 0.6;
+      }
+
+      .div {
+        display: inline-block;
+        width: 85%;
+        border-radius: 6px;
+        background-color: #fff;
+        height: 60px;
+        line-height: 60px;
+        text-align: center;
+      }
+
+      .disableClass {
+        background-color: #e5e5e5;
+      }
+    }
+
+    .buttons {
+      position: absolute;
+      bottom: 0;
+
+      text-align: center;
+      right: 1%;
+
+      .buttons-div {
+        // min-width: 260px;
+        text-align: center;
+        float: right;
+        margin-top: 10px;
+        display: flex;
+      }
+
+      .first-button {
+        width: 90%;
+        // background-color: #adb3bc;
+        position: relative;
+      }
+
+      .second-button {
+        width: 132px;
+        height: 60px;
+        margin-left: 16px;
+        position: relative;
+      }
+    }
+  }
+
+  .icon-delete-character {
+    font-size: 32px;
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,45 +1,46 @@
 <template>
   <view class="index">
     <view>
-      <img src="" alt="">
+      <img src alt />
     </view>
-    {{ msg }}
+    {{ state.msg }}
     <view class="btn">
-      <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
+      <nut-button type="primary" @click="handleClick('text', state.msg2, true)">点我</nut-button>
     </view>
-    <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+    <keyboard-car :show="state.show" />
+    <nut-toast
+      :msg="state.msg"
+      v-model:visible="state.show"
+      :type="state.type"
+      :cover="state.cover"
+    />
   </view>
 </template>
 
 <script lang="ts">
-import { reactive, toRefs } from 'vue';
 export default {
-  name: 'Index',
-  components: {
-    
-  },
-  setup(){
-    const state = reactive({
-      msg: '欢迎使用 NutUI3.0 开发小程序',
-      msg2: '你成功了～',
-      type: 'text',
-      show: false,
-      cover: false
-    });
-
-    const handleClick = (type: string, msg: string, cover: boolean = false) => {
-      state.show = true;
-      state.msg2 = msg;
-      state.type = type;
-      state.cover = cover;
-    };
-
-    return {
-      ...toRefs(state),
-      handleClick
-    }
-  }
+  name: 'Index'
 }
+</script>
+
+<script lang="ts" setup>
+import { reactive, } from 'vue';
+import KeyboardCar from '../../component/keyboard-car.vue';
+
+const state = reactive({
+  msg: '欢迎使用 NutUI3.0 开发小程序',
+  msg2: '你成功了～',
+  type: 'text',
+  show: false,
+  cover: false,
+});
+
+const handleClick = (type: string, msg: string, cover: boolean = false) => {
+  state.show = !state.show;
+  state.msg2 = msg;
+  state.type = type;
+  state.cover = cover;
+};
 </script>
 
 <style lang="scss">
